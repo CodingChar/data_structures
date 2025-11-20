fbea95690c30fbba7675d8191a86617719a5841f
CodingChar <chrsa23@gmail.com>
2025-11-20 12:53:07 -0400

    convertIntToArray fix

--- week1/MasterMind.cpp
+++ week1/MasterMind.cpp
@@ -15,19 +15,15 @@ using namespace std;
 
 */
 
-void genCode(int* codeArr)
+void genCode(int *codeArr) // O(6) -> O(1)
 {
-
     /*
 
         Shuffling Based 4 digits Code Generator Algorithm, digits between 1-6
 
-        void genCode(int (&codeArr)[4] ) -> recieves an array  as a argument
-        modifies the array given through his original reference
-        
+        void genCode(int* codeArr) -> recieves an int pointer that is supposed to point to an array
         then modifies the array using the arrays original reference, so the array obtaints the code
         through the non-biased-shuffle algorithm.
-
     */
     /*
         Despues de medio dia, porfin un algoritmo de generacion
@@ -40,18 +36,16 @@ void genCode(int* codeArr)
 
     generator.seed(time(0)); // Sets the seed, different seed each time the program runs
 
-
     for (int i = 0; i < 6; i++)
     {
         // Temporary variable, helps to the shuffle proccess
-        int temp = numbers[i]; //3
+        int temp = numbers[i]; // 3
         // generates a random number based in the distribution
         std::uniform_int_distribution<int> distribution(0, i); // Sets the range //0, 3
-        int swapIndex = distribution(generator); //0, 3, 2;
+        int swapIndex = distribution(generator);               // 0, 3, 2;
         numbers[i] = numbers[swapIndex];
         numbers[swapIndex] = temp;
     }
-
 
     codeArr[0] = numbers[0];
     codeArr[1] = numbers[1];
@@ -59,85 +53,120 @@ void genCode(int* codeArr)
     codeArr[3] = numbers[3];
 }
 
-void converIntToArray(int (&arr)[], int number, int size)
+void converIntToArray(int *arr, int number, int size) // O(N)
 {
-    size = size -1; 
+    /*
+
+        The function  recieve 3 arguments which are the followed
+
+        # - int *arr -> a pointer that points to a int, expected to be the array
+        where we gonna store the digits succesfully
+
+        # - int number -> the number itselft that is going to be separated in its digits
+
+        # - int size ->  the size of the array given as first argument, to know how many digits
+        does the programmer wants to store inside
+    */
+    // Pointer validations
+    if (arr == nullptr)
+    {
+        cerr << endl
+             << "null pointer given as first argument" << endl;
+        return;
+    }
+
+    size = size - 1;
     while (size >= 0)
     {
         arr[size] = number % 10;
         number = number / 10;
         size = size - 1;
     }
-}   
+}
 
 int main()
 {
     int code[4] = {0, 0, 0, 0};
-    int guess[4] = {0, 0, 0, 0}; 
+    int guess[4] = {0, 0, 0, 0};
 
     int attemptsLeft = 10;
     int userInput = 0;
 
-    int totalC = 0; //If totalC reaches 4, assume that the user guessed right;
+    int totalC = 0; // If totalC reaches 4, assume that the user guessed right;
 
     cout << "\t\t \n \n Welcome To MasterMind \n \n \t\t";
-    
+
     cin.clear();
 
-
-    genCode(code); 
+    genCode(code);
 
     cout << "\n\n Code Generated \n\n";
-    cout<<"Code:"<<endl<<code[0]<<code[1]<<code[2]<<code[3]<<endl;
-    cout<<"\n\nGuess the 4 digits code to win\n\n";
+    cout << "Code:" << endl
+         << code[0] << code[1] << code[2] << code[3] << endl;
+    cout << "\n\nGuess the 4 digits code to win\n\n";
     do
     {
-        cout<<"\n\nCode(Attempts Left: "<<attemptsLeft<<")"<<":";        
-        if(!(cin>>userInput))
+        cout << "\n\nCode(Attempts Left: " << attemptsLeft << ")" << ":";
+
+        // Input validation
+        if (!(cin >> userInput))
         {
             cout << "\n\n Invalid code, not a number\n\n";
             cin.clear();
             cin.ignore(1000, '\n');
-            userInput=0;
+            userInput = 0;
             continue;
         }
 
-        if(userInput <= 0 || userInput >= 6666){
-            cout << "\n\n Not valid combination\n\n";            
-        }else{
-            converIntToArray(guess, userInput, 4); //O(n);
-            cout<<'\n';
-            for(int gi=0; gi<4; gi++){ //O(4) //gi = Guess Index
-                for(int ci=0; ci<4; ci++){ //O(4) //ci  = Code Index
-                    //cout<<endl<<"Guess:"<<guess[gi]<<endl<<"Code:"<<code[ci]<<endl;
-                    if(gi==ci && guess[gi]==code[ci]){
-                        cout<<'C';
-                        totalC+=1; 
+        if (userInput <= 0 || userInput >= 6666)
+        {
+            cout << "\n\n Not valid combination\n\n";
+        }
+        else
+        {
+            converIntToArray(guess, userInput, 4); // O(n);
+            cout << '\n';
+
+            for (int gi = 0; gi < 4; gi++)     // O(4)
+            {                                  // gi = Guess Index
+                for (int ci = 0; ci < 4; ci++) // O(4)
+                {                              // O(4) //ci  = Code Index
+                    if (gi == ci && guess[gi] == code[ci])
+                    {
+                        cout << 'C';
+                        totalC += 1;
                         break;
-                    }else if(guess[gi] == code[ci]){
-                        cout<<'F';
+                    }
+                    else if (guess[gi] == code[ci])
+                    {
+                        cout << 'F';
                         break;
-                    }else if(ci==3){
-                        cout<<'X';
+                    }
+                    else if (ci == 3)
+                    {
+                        cout << 'X';
                         break;
                     }
                 }
-            }              
+            }
         }
-        
+
         userInput = 0;
 
-        
-        if(totalC >= 4){
-            cout<<"\n\nYou Scored ( "<<10-(10-attemptsLeft)<<" )"<<" points out of ( 10 )";
+        if (totalC >= 4)
+        {
+            cout << "\n\nYou Scored ( " << 10 - (10 - attemptsLeft) << " )" << " points out of ( 10 )";
             break;
-        }else{
-            cout<<"\n";
-            totalC=0;
-            attemptsLeft-=1;
         }
-    } while (attemptsLeft > 0);
-    if(attemptsLeft<=0){
-        cout<<"\n\n You lose";
+        else
+        {
+            cout << "\n";
+            totalC = 0;
+            attemptsLeft -= 1;
+        }
+    } while (attemptsLeft > 0); // 0(10)
+    if (attemptsLeft <= 0)
+    {
+        cout << "\n\n You lose";
     }
 }
